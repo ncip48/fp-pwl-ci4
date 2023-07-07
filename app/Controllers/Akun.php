62168812d4c9dae44c4a25f79509c69d15860d5c
Azizaja <azizsetiawan@students.amikom.ac.id>
2023-07-08 03:56:54 +0700

    profil

--- app/Controllers/Akun.php
+++ app/Controllers/Akun.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+
 use App\Controllers\BaseController;
 
 class Akun extends BaseController
@@ -13,5 +15,25 @@ class Akun extends BaseController
             'title' => 'Profil',
         ];
         return view('akun', $data);
+    }
+
+    public function editPass()
+    {
+        //create function to edit password here
+
+        $data = [
+            'title' => 'Ubah Password',
+        ];
+        return view('editPass', $data);
+    }
+
+    public function editFoto()
+    {
+        //create function to edit foto here
+
+        $data = [
+            'title' => 'Ubah Foto',
+        ];
+        return view('editFoto', $data);
     }
 }
